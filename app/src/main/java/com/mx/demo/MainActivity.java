6cca4264ae2b819c6d0a9246ab7dfa3e5eef7095
zmx_f <zmx_final@163.com>
2016-12-29 15:02:43 +0800

    更新一下

--- app/src/main/java/com/mx/demo/MainActivity.java
+++ app/src/main/java/com/mx/demo/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CacheParams cacheParams = new CacheParams("/sdcard/cache");
+        CacheParams cacheParams = new CacheParams("/sdcard/cache1");
         cacheParams.setDiskCacheCount(100);
         cacheParams.setMemCacheSizePercent(this, 0.06f);
 
